3ea249b9ce928539fc1a1b188b9bb1649cedd00a
Yogurt7v <egorov-2k@yandex.ru>
2023-12-16 15:07:40 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <h2>{value}</h2>
+      <div className="buttons">
+        <button className="buttonPlus" onClick={() => setValue(value + 1)}>
+          Increment
+        </button>
+        <button className="buttonMinus" onClick={() => setValue(value - 1)}>
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
